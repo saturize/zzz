fe17e40c8a073e50465fe578a623d223004156f3
Saturize <grfhfgthnjdtgeyh@gmail.com>
2024-11-03 22:17:13 +0100

    -

--- commands/info/help.js
+++ commands/info/help.js
@@ -2,44 +2,49 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('
 const path = require('path');
 const config = require(path.join(__dirname, '../../config.json'));
 
+const createHelpButtons = () => {
+    const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('mod')
+                .setEmoji('<:moderation:1302696364270026853>'),
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('info')
+                .setEmoji('<:info:1302694232879796265>'),
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('fun')
+                .setEmoji('<:fun:1302695666182520874>'),
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('interactions')
+                .setEmoji('<:interact:1302695110408011776>'),
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('settings')
+                .setEmoji('<:settings:1302714616958418995> ')
+        );
+
+    const row2 = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId('helpEmbed')
+                .setEmoji('<:help:1302718126831570964>')
+        );
+
+    return [row, row2];
+};
+
+
 module.exports = {
     name: 'help',
 
     run: async (client, message, args) => {
         
         const { moderation, info, fun, interact, settings, help, category } = client.customEmojis;
-
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('mod')
-                    .setEmoji('<:moderation:1302696364270026853>'),
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('info')
-                    .setEmoji('<:info:1302694232879796265>'),
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('fun')
-                    .setEmoji('<:fun:1302695666182520874>'),
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('interactions')
-                    .setEmoji('<:interact:1302695110408011776>'),
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('settings')
-                    .setEmoji('<:settings:1302714616958418995> '),
-            );
-
-        const row2 = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setStyle(ButtonStyle.Secondary)
-                    .setCustomId('helpEmbed')
-                    .setEmoji('<:help:1302718126831570964>')
-            );
 
         const helpEmbed = new EmbedBuilder()
             .setTitle(".gg/saturize")
@@ -59,6 +64,8 @@ module.exports = {
             })
             .setTimestamp()
 
+        const [row, row2] = createHelpButtons();
+
         await message.reply({
             embeds: [helpEmbed],
             components: [row, row2]
@@ -66,47 +73,51 @@ module.exports = {
     },
 
 
-helpInteraction: async (interaction) => {
-    const embedMap = {
-        mod: new EmbedBuilder()
-            .setTitle("Moderation Commands")
-            .setColor(config.embedColor)
-            .setDescription("Liste des commandes de modération...\n..."),
+    helpInteraction: async (interaction) => {
         
-        info: new EmbedBuilder()
-            .setTitle("Information Commands")
-            .setColor(config.embedColor)
-            .setDescription("Liste des commandes d'information...\n..."),
-        
-        fun: new EmbedBuilder()
-            .setTitle("Fun Commands")
-            .setColor(config.embedColor)
-            .setDescription("Liste des commandes fun...\n..."),
-        
-        interactions: new EmbedBuilder()
-            .setTitle("Interactions Commands")
-            .setColor(config.embedColor)
-            .setDescription("Liste des commandes d'interactions...\n..."),
-        
-        settings: new EmbedBuilder()
-            .setTitle("Settings Commands")
-            .setColor(config.embedColor)
-            .setDescription("Liste des commandes de paramètres...\n..."),
-        
-        helpEmbed: new EmbedBuilder()
-            .setTitle("Help")
-            .setColor(config.embedColor)
-            .setDescription("Besoin d'aide ? Voici les catégories de commandes...\n...")
-    };
+        const { moderation, info, fun, interact, settings, help, category } = client.customEmojis;
 
-    const selectedEmbed = embedMap[interaction.customId];
+        const embedMap = {
+            mod: new EmbedBuilder()
+                .setTitle("Moderation Commands")
+                .setColor(config.embedColor)
+                .setDescription("Liste des commandes de modération...\n..."),
+        
+            info: new EmbedBuilder()
+                .setTitle("Information Commands")
+                .setColor(config.embedColor)
+                .setDescription("Liste des commandes d'information...\n..."),
+        
+            fun: new EmbedBuilder()
+                .setTitle("Fun Commands")
+                .setColor(config.embedColor)
+                .setDescription("Liste des commandes fun...\n..."),
+        
+            interactions: new EmbedBuilder()
+                .setTitle("Interactions Commands")
+                .setColor(config.embedColor)
+                .setDescription("Liste des commandes d'interactions...\n..."),
+        
+            settings: new EmbedBuilder()
+                .setTitle("Settings Commands")
+                .setColor(config.embedColor)
+                .setDescription("Liste des commandes de paramètres...\n..."),
+        
+            helpEmbed: new EmbedBuilder()
+                .setTitle("Help")
+                .setColor(config.embedColor)
+                .setDescription("Besoin d'aide ? Voici les catégories de commandes...\n...")
+        };
 
-    if (selectedEmbed) {
-        await interaction.update({ embeds: [selectedEmbed], components: [] }).catch(err => {
-            console.error('Erreur lors de la mise à jour de l\'interaction:', err);
-        });
-    } else {
-        await interaction.reply({ content: "Commande non reconnue.", ephemeral: true });
+        const selectedEmbed = embedMap[interaction.customId];
+        const [row, row2] = createHelpButtons();
+
+        if (selectedEmbed) {
+            await interaction.update({ embeds: [selectedEmbed], components: [row, row2] }).catch(err => {
+                console.error('Erreur lors de la mise à jour de l\'interaction:', err);
+            });
+        } else {
+            await interaction.reply({ content: "Commande non reconnue.", ephemeral: true });
+        }
     }
-}
 };
