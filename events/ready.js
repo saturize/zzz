2f308f3ca5ea3f57080180faebc95df7599b764e
Jehyann <grfhfgthnjdtgeyh@gmail.com>
2025-05-17 00:10:14 +0200

    changed activity and status

--- events/ready.js
+++ events/ready.js
@@ -3,18 +3,11 @@ const { ActivityType } = require('discord.js');
 module.exports = async (client) => {
     console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
 
-
-        client.user.setActivity('error', {
-            type: ActivityType.Streaming,
-            url: 'https://www.twitch.tv/saturize'
-        });
-    
-        client.user.setPresence({
-            status: 'dnd',
-            // activities: [
-            //    { name: '.gg/saturize', type: ActivityType.Playing }
-            //]
-        });
+    // Définir le statut DND sans activité
+    client.user.setPresence({
+        status: 'dnd',
+        activities: [] // Tableau vide = pas d'activité
+    });
 
     try {
         await client.guilds.fetch();
