1335b1a8569115811e1e143f0c8c7474e2475066
Jehyann <grfhfgthnjdtgeyh@gmail.com>
2025-07-17 01:53:00 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
     const ping = sentMessage.createdTimestamp - message.createdTimestamp;
     const apiLatency = Math.round(client.ws.ping);
 
-    await sentMessage.edit(`<:connection:1302809154896793640>  Pong !\nLatence du message : **${ping}ms**\nLatence de l'API : **${apiLatency}ms**`);
+    await sentMessage.edit(`<:connection:1394845385029189674>  Pong !\nLatence du message : **${ping}ms**\nLatence de l'API : **${apiLatency}ms**`);
 };
 
 exports.name = "ping";
