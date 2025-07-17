27c0de6520d4857c226127a1ba0d58f4f38f5e72
Jehyann <grfhfgthnjdtgeyh@gmail.com>
2025-07-17 02:57:59 +0200

    Create planning.js

---
+++ commands/stream/planning.js
@@ -0,0 +1,10 @@
+const { EmbedBuilder } = require('discord.js');
+const path = require('path');
+const config = require(path.join(__dirname, '../../config.json'));
+
+exports.run = async (client, message, args) => {
+
+};
+
+exports.name = "planning";
+exports.description = "Affiche mon planning.";
