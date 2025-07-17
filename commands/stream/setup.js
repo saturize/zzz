fab7b7171a15c04d1602a7f1ee5153b417d31d71
Jehyann <grfhfgthnjdtgeyh@gmail.com>
2025-07-17 03:32:25 +0200

    Update setup.js

--- commands/stream/setup.js
+++ commands/stream/setup.js
@@ -10,7 +10,6 @@ exports.run = (client, message, args) => {
 \`GPU :\` MSI GeForce RTX3070
 \`CPU :\` AMD RYZEN 7 3700X
 \`Watercooling :\` MSI CORELIQUID 240R
-
 # Set-up bureau
 \`Ecran principal :\` MSI incurvé 27"
 \`Ecran secondaire :\` Samsung 24"
@@ -19,7 +18,6 @@ exports.run = (client, message, args) => {
 \`Caméra :\` Logitech C920
 \`Clavier :\` Razer ornata V2
 \`Micro :\` Rode NT1
-
 # Set-up VR
 Valve Index
 Oculus Quest 2
